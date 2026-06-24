fea85de68029a0a606be9cf71c30bc22b465e6d1
nino <ninojacobs1@gmail.com>
2026-06-24 10:37:12 +0200

    changes

--- src/main/java/com/capitec/securefile/controller/CustomerStatementsController.java
+++ src/main/java/com/capitec/securefile/controller/CustomerStatementsController.java
@@ -16,7 +16,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.format.annotation.DateTimeFormat;
 
 import java.time.LocalDate;
 import java.util.List;
--- src/main/java/com/capitec/securefile/database/entity/Statement.java
+++ src/main/java/com/capitec/securefile/database/entity/Statement.java
@@ -1,5 +1,5 @@
 package com.capitec.securefile.database.entity;
-import jakarta.persistence.Column;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
